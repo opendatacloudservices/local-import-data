f14a049ee3b01752cf5e12b20b4e7784485a3a4a
Sebastian Meier <contact@sebastianmeier.eu>
2020-11-13 16:04:20 +0100

    test dummy database

--- test/test.js
+++ test/test.js
@@ -1,6 +1,12 @@
 const dotenv = require('dotenv');
 const pg = require('pg');
-const { forEachChild } = require('typescript');
+const {
+  masterTableExist: ckanMasterTableExist,
+  initMasterTable: ckanInitMasterTable,
+  tablesExist: ckanTablesExist,
+  initTables: ckanInitTables,
+  definition_tables: ckanDefinition_tables,
+} = require('../node_modules/local-ckan-harvester/build/postgres/index');
 
 const {
   tablesExist,
@@ -100,4 +106,78 @@ test('masterTableDrop', async () => {
     });
 });
 
-// Run tests for all ckan portals in the master db (different test file)
+/*---------- HARVESTER TESTS ----------*/
+
+// CKAN is used to also test harvester class functions,
+// subsequent harvesters do not require class function tests
+
+/*----- CKAN -----*/
+
+// connect to postgres (via env vars params)
+const ckanClient = new pg.Client({
+  user: process.env.PGCKANUSER,
+  host: process.env.PGCKANHOST,
+  database: process.env.PGCKANDATABASETEST,
+  password: process.env.PGCKANPASSWORD,
+  port: process.env.PGCKANPORT,
+});
+
+ckanClient.connect();
+
+test('CKAN: check if test database is available', async () => {
+  await ckanClient.query('SELECT version() AS v;').then((result) => {
+    expect(result).toHaveProperty('rows');
+    expect(result.rows.length).toBeGreaterThan(0);
+    expect(typeof result.rows[0].v).toBe('string');
+  });
+
+  // reset demo database
+  await ckanClient.query(`SELECT 
+      tablename
+    FROM
+      pg_tables
+    WHERE
+      schemaname = 'public';
+  `).then((result) => {
+    return Promise.all(result.rows.map((row) => {
+      return ckanClient.query(`DROP TABLE IF EXISTS ${row.tablename} CASCADE;`);
+    }));
+  });
+
+  await ckanMasterTableExist(ckanClient)
+    .then((result) => {
+      if (!result) {
+        return ckanInitMasterTable(ckanClient);
+      }
+    });
+
+  await ckanMasterTableExist(ckanClient)
+    .then((result) => {
+      expect(result).toBe(true);
+    });
+
+  await ckanTablesExist(ckanClient, 'govdata', ckanDefinition_tables)
+    .then((result) => {
+      if (!result) {
+        return ckanInitTables(ckanClient, 'govdata', 'ckan.govdata.de/api/3', 3, null);
+      }
+      return Promise.resolve();
+    });
+
+  await ckanTablesExist(ckanClient, 'govdata', ckanDefinition_tables)
+    .then((result) => {
+      expect(result).toBe(true);
+    });
+
+  // add some demo data
+  await ckanClient.query(`INSERT INTO govdata_packages (id, title, metadata_modified, metadata_created, license_title, author, maintainer, ckan_status) VALUES (1, 'title', '2020-01-01 00:00:00', '2020-01-01 00:00:00', 'license_title', 'author', 'maintainer', 'new')`);
+  await ckanClient.query(`INSERT INTO govdata_groups (id, title) VALUES (1, 'title')`);
+  await ckanClient.query(`INSERT INTO govdata_tags (id, display_name) VALUES (1, 'title')`);
+  await ckanClient.query(`INSERT INTO govdata_resources (id, name, format, url, license, size) VALUES (1, 'name', 'format', 'url', 'license', 500)`);
+  await ckanClient.query(`INSERT INTO govdata_ref_groups_packages (package_id, group_id) VALUES (1, 1)`);
+  await ckanClient.query(`INSERT INTO govdata_ref_tags_packages (package_id, tag_id) VALUES (1, 1)`);
+  await ckanClient.query(`INSERT INTO govdata_ref_resources_packages (package_id, resource_id) VALUES (1, 1)`);
+});
+
+
+
